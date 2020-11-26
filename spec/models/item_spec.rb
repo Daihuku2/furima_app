9f5d538719a0bfcdcea210c1fdd8fb6dcc30fe2e
Daihuku2 <70841018+Daihuku2@users.noreply.github.com>
2020-11-26 19:01:44 +0900

    商品出品機能修正２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,45 +37,75 @@ RSpec.describe Item, type: :model do
       end
 
       it "価格が299では登録できないこと" do
-        @item.price = "299"
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than 299")
       end
 
-      it "価格が全角数字では登録できないこと" do
-        @item.price = "５００"
+      it "価格が10000001では登録できないこと" do
+        @item.price = 10000001
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
       end
 
       it "category_idが1では登録できないこと" do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
 
+      it "category_idが空では登録できないこと" do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+      end
+
       it "status_idが1では登録できないこと" do
-        @item.status_id = "1"
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
 
+      it "status_idが空では登録できないこと" do
+        @item.status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank", "Status is not a number")
+      end
+
       it "burden_idが1では登録できないこと" do
-        @item.burden_id = "1"
+        @item.burden_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Burden must be other than 1")
       end
 
+      it "burden_idが空では登録できないこと" do
+        @item.burden_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Burden can't be blank", "Burden is not a number")
+      end
+
       it "area_idが1では登録できないこと" do
-        @item.area_id = "1"
+        @item.area_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Area must be other than 1")
       end
 
+      it "area_idが空では登録できないこと" do
+        @item.area_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank", "Area is not a number")
+      end
+
       it "delivery_idが1では登録できないこと" do
-        @item.delivery_id = "1"
+        @item.delivery_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery must be other than 1")
+      end
+
+      it "delivery_idが空では登録できないこと" do
+        @item.delivery_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery can't be blank", "Delivery is not a number")
       end
     end
 end
