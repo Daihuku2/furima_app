86eab06ca74cb1b480bb47d773ad8506c85ae8ef
Daihuku2 <70841018+Daihuku2@users.noreply.github.com>
2020-12-02 11:37:53 +0900

    商品削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
     before_action :authenticate_user!, only:[:new]
-    before_action :set_tweet, only: [:edit, :show, :update]
+    before_action :set_tweet, only: [:edit, :show, :update, :destroy]
     before_action :move_to_index, only: [:edit]
 
     def index
@@ -36,7 +36,6 @@ class ItemsController < ApplicationController
     end
 
     def destroy
-        @item = Item.find(params[:id])
         @item.destroy
         redirect_to root_path
     end
